65e2db94b85f8058d32a9365ac9e0c1e8b8b2957
Juliano Biffi <julianobiffidev@gmail.com>
2025-10-04 21:39:41 -0300

    Creating DDD+CQRS Folders

--- TelegramBotManager/Functions/FinancialControl.cs
+++ TelegramBotManager/Functions/FinancialControl.cs
@@ -1,22 +1,21 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace TelegramBotManager.Functions;
 
-public class Function1
+/// <summary>
+/// Manages financial control operations with telegram bot.
+/// </summary>
+public class FinancialControl(ILogger<FinancialControl> _logger, IConfiguration _configuration)
 {
-    private readonly ILogger<Function1> _logger;
-
-    public Function1(ILogger<Function1> logger)
-    {
-        _logger = logger;
-    }
-
-    [Function("Function1")]
+    [Function("FinanceControlMessage")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
+        string botToken = _configuration.GetSection("FinancialControl")["TelegramBotToken"];
+
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
     }
--- TelegramBotManager/Program.cs
+++ TelegramBotManager/Program.cs
@@ -1,14 +1,21 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var builder =
+    FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
+builder.Configuration
+       .SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("local.settings.json", optional: true, reloadOnChange: false)
+       .AddEnvironmentVariables();
+
 builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
+       .AddApplicationInsightsTelemetryWorkerService()
+       .ConfigureFunctionsApplicationInsights();
 
 builder.Build().Run();
