7db77f29623423e2fde8d262fccd65096e89589c
Juliano R. Biffi <39343841+JulianoBiffi@users.noreply.github.com>
2026-02-04 10:37:52 -0300

    feat: add TransactionExists to ITransactionRepository

--- TelegramBotManager/Domain/Interfaces/ITransactionRepository.cs
+++ TelegramBotManager/Domain/Interfaces/ITransactionRepository.cs
@@ -1,4 +1,4 @@
-﻿using TelegramBotManager.Domain.Entities.FinancialControl;
+using TelegramBotManager.Domain.Entities.FinancialControl;
 
 namespace TelegramBotManager.Domain.Interfaces;
 
@@ -9,4 +9,5 @@ public interface ITransactionRepository
     Task<Transaction> SaveAsync(Transaction transaction, CancellationToken cancellationToken);
     Task<decimal> GetAmmountOfMonth(Transaction transaction, CancellationToken cancellationToken, bool filterByCategory = false);
     Task<List<Transaction>> GetTransactionsByPeriod(DateTime startDate, DateTime endDate, CancellationToken cancellationToken);
+    Task<bool> TransactionExists(DateTime date, decimal value, string description, CancellationToken cancellationToken);
 }
