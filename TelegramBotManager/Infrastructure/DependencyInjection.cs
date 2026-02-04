a37edbf0456d4504291134c95299fd0af2d4d79a
Juliano R. Biffi <39343841+JulianoBiffi@users.noreply.github.com>
2026-02-04 12:47:44 -0300

    feat: register IBankTransactionParser implementations in DI

--- TelegramBotManager/Infrastructure/DependencyInjection.cs
+++ TelegramBotManager/Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using TelegramBotManager.Application.Interfaces;
+using TelegramBotManager.Application.Parsers;
 using TelegramBotManager.Domain.Interfaces;
 using TelegramBotManager.Infrastructure.Repositories;
 
@@ -10,5 +12,11 @@ public static class DependencyInjection
     {
         services.AddScoped<ICategoryRepository, CategoryRepository>();
         services.AddScoped<ITransactionRepository, TransactionRepository>();
+
+        // Bank notification parsers
+        services.AddScoped<IBankTransactionParser, NubankTransactionParser>();
+        services.AddScoped<IBankTransactionParser, PortoTransactionParser>();
+        services.AddScoped<IBankTransactionParser, BBTransactionParser>();
+        services.AddScoped<IBankTransactionParser, GoogleWalletTransactionParser>();
     }
 }
